d48d997b5e380fa93ed801280a2b67aca214274a
Ibrahim Mansoor Khalid <ibrahimmkhalid@gmail.com>
2024-04-27 00:28:47 -0700

    update

--- make_small.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-head -n 57 probe_known_attacks.arff | tail -n 51 | sed -E "s/@ATTRIBUTE '([a-zA-Z_]*)'.*/\1,/" | tr -d '\n'  | sed -E "s/class,/class\n/" > probe_known_attacks_small.csv
-cat probe_known_attacks.csv | grep -C 5 "attack" --no-group-separator >> probe_known_attacks_small.csv
